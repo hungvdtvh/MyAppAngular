f969bedbb8d76b54c29784a2fe56029ae317be3d
DuyHung <hungvd.tvh@gmail.com>
2021-09-29 15:19:08 +0700

    ngFor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,15 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit { 
   public name: string ='Võ Duy Hưng';
   public age: number = 1994;
+  public skills: any=[];
   constructor() { }
 
   ngOnInit(): void {
-
+    this.skills.push("C#","JavaScript","Python","Angular");
+    console.log(this.skills);
     } 
   public resetName(): void{
     //console.log("ResetName")
     this.name="";
-
   }
   
 }
