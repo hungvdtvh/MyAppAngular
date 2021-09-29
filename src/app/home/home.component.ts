179d5918795b201586ec56c3a269577c7668cc5a
DuyHung <hungvd.tvh@gmail.com>
2021-09-29 08:54:58 +0700

    Module

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,15 +5,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-  public name: string ="Võ Duy Hưng";
+export class HomeComponent implements OnInit { 
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-  public ResetName(){
-    this.name="";
-  }
+  } 
 
 }
