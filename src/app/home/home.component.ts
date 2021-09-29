401e0b4f17bd18766c7aeaa42b0c2cb4074335f6
DuyHung <hungvd.tvh@gmail.com>
2021-09-29 13:37:19 +0700

    Change Home Component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit { 
   public name: string ='Võ Duy Hưng';
-
+  public age: number = 1994;
   constructor() { }
 
-  ngOnInit(): void {
-  } 
-
+  ngOnInit(): void {  } 
+  
 }
