0ac7c622e6845d8f43d4931020c59e58a1ac5283
DuyHung <hungvd.tvh@gmail.com>
2021-10-05 10:10:04 +0700

    Switch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ExponentialStrengthPipe } from './exponential-strength.pipe';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    ExponentialStrengthPipe
+    ExponentialStrengthPipe,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
